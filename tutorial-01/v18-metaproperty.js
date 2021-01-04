ed66b85930f13de11fcba0e312f7fd4088f4dd10
githubdmn <dmninbox@gmail.com>
2021-01-04 17:10:44 +0100

    TODO: new.target metaproperty

--- tutorial-01/v18-metaproperty.js
+++ tutorial-01/v18-metaproperty.js
@@ -1,9 +1,25 @@
 
 
 
+// first attempt
+//function Person(name) {
+//	this.name = name;
+//}
+
+function Person(name) {
+	if (typeof new.target === Person) {
+		this.name = name;
+	} else {
+		throw new Error('new keyword is missing');
+	}
+}
 
 
+let person = new Person('Anna');
+// let someObject =  Person('Anna');
+// let second = Person.call(person, 'John')
 
+console.log(person, " ", someObject );
 
 
 
