f75e538a253f677810a2d93f7b38ae7e22c3ce64
githubdmn <dmninbox@gmail.com>
2021-03-31 09:31:52 +0200

    ES5 vs ES6 prototypes

--- tutorial-01/v25-prototype-es6.js
+++ tutorial-01/v25-prototype-es6.js
@@ -1,5 +1,4 @@
 
-
 console.log("Prototype - ES6 enhancement\n\n");
 
 
@@ -10,25 +9,50 @@ let Letter = {
 	}
 };
 
-let Number = {
+let NumberValue = {
 	getValue() {
-		return 1;
+		return 3;
 	}
 };
 
-
+//ES5 syntax
 let myLetter = Object.create(Letter);
 console.log(`my letter -> ${myLetter.getValue()}`);
 // console.log(myLetter.getValue());
 console.log(Object.getPrototypeOf(myLetter));
+console.log(Object.getPrototypeOf(myLetter) === Letter);
 
 
-Object.setPrototypeOf(myLetter, Number);
+Object.setPrototypeOf(myLetter, NumberValue);
 console.log(`my letter -> ${myLetter.getValue()}`);
 
-//TODO
+// ES5 syntax
+
+let myNewLetter = Object.create(Letter);
+
+let newLetter = {
+	getValue() {
+		return Object.getPrototypeOf(this).getValue(this) + " ES5";
+	}
+};
+
+
+Object.setPrototypeOf(newLetter, myNewLetter);
+console.log(newLetter.getValue());
 
 
 
 
+//TODO -> ES6 syntax
+// Access the parent properties
+let myLetterES6 = Object.create(Letter);
 
+let newLetterES6 = {
+	getValue() {
+		return super.getValue() + " ES6";
+	}
+};
+
+
+Object.setPrototypeOf(newLetterES6, myLetterES6);
+console.log(newLetterES6.getValue());
