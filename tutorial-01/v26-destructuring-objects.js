47552ca239cd868841df255f24444756d8a111e7
githubdmn <dmninbox@gmail.com>
2021-04-05 14:22:38 +0200

    destructuring objects - basic

--- tutorial-01/v26-destructuring-objects.js
+++ tutorial-01/v26-destructuring-objects.js
@@ -1,1 +1,17 @@
 console.log("\n Destructuring objects \n");
+
+let options = {
+    repeat: true,
+    save: true
+}
+
+let repeat_es5 = options.repeat;
+let save_es5 = options.save;
+console.log(`ES5 ${repeat_es5} and ${save_es5}\n`);
+
+
+let { repeat, save } = options;
+console.log(`ES6 ${repeat} and ${save}\n`);
+
+
+
