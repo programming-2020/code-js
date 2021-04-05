39125c7e0c6037681b6cf7f140a8f088081ca7a1
githubdmn <dmninbox@gmail.com>
2021-04-05 14:48:35 +0200

    destructuring objects - basic

--- tutorial-01/v26-destructuring-objects.js
+++ tutorial-01/v26-destructuring-objects.js
@@ -14,4 +14,63 @@ let { repeat, save } = options;
 console.log(`ES6 ${repeat} and ${save}\n`);
 
 
+let node = {
+    type: 'My type',
+    mark: 'My mark'
+};
+let type = "My other type";
+let mark = 11;
+
+// SyntaxError: Identifier 'type' has already been declared
+// let { type, name } = node;
+// console.log(`ES6 ${type} and ${name}\n`);
+
+// Solution 
+({ type, mark } = node); // Keeps the initial value
+console.log(`ES6 ${type} and ${mark}\n`);
+
+
+let item = {
+    id: 'My 1 id',
+    label: 'My 1 label'
+};
+let id = "My other id";
+let label = "Label 11";
+
+function myFunction(value) {
+    console.log(value);
+}
+
+myFunction({ id, label } = item); // assigned value cannot be null or undefined
+console.log(`ES6 ${id} and ${label}\n`);
+
+myFunction({ id, label, isValid } = item);
+myFunction({ id, label, isValid: localValid = false } = item);
+
+
+
+console.log(`\nES6 nested structure \n\n`);
+
+const mainObject = {
+    first: "My First Property",
+    second: "My second Property",
+    third: {
+        start: {
+            line: 1,
+            column: 2
+        },
+        end: {
+            line: 4,
+            column: 4
+        }
+    }
+}
+
+let { third: { start } } = mainObject;
+console.log(`ES6 nested 1st - ${start.line} and ${start.column}`);
+
+
+let { third: { end: { line, column } } } = mainObject;
+console.log(`ES6 nested 2nd - ${line} and ${column}`);
+
 
